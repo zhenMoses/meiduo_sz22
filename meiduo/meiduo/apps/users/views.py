064ca4d136c07814786520811af7ac4b280ad170
chao <chao@itcast.cn>
2019-01-19 16:01:55 +0800

    判断用户名是否已存在

--- meiduo/meiduo/apps/users/views.py
+++ meiduo/meiduo/apps/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
+from .models import User
+
 # Create your views here.
 
 # POST /users/
@@ -9,3 +13,19 @@ class UserView(CreateAPIView):
     """用户注册"""
     # 指定序列化器
     serializer_class = UserSerializer
+
+
+# url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
+class UsernameCountView(APIView):
+    """验证用户名是否已存在"""
+
+    def get(self, request, username):
+        # 查询用户名是否已存在
+        count = User.objects.filter(username=username).count()
+
+        # 构建响应数据
+        data = {
+            'count': count,
+            'username': username
+        }
+        return Response(data)
