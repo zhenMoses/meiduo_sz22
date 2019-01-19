e29d608c530ecfc3c3c708429aeca889307554f9
chao <chao@itcast.cn>
2019-01-19 11:19:46 +0800

    解决60s内同一个手机号重复发送短信问题

--- meiduo/meiduo/apps/verifications/views.py
+++ meiduo/meiduo/apps/verifications/views.py
@@ -5,6 +5,7 @@ from django_redis import get_redis_connection
 from meiduo.libs.yuntongxun.sms import CCP
 from rest_framework.response import Response
 import logging
+from rest_framework import status
 
 logger = logging.getLogger('django')  # 创建日志输出器
 
@@ -15,15 +16,27 @@ class SMSCodeView(APIView):
     """发送短信验证码"""
 
     def get(self, request, mobile):
-        # 1.生成短信验证码
+
+        # 0.创建redis连接对象
+        redis_conn = get_redis_connection('verify_codes')
+        # 1.获取此手机号是否有发送过的标记
+        flag = redis_conn.get('send_flag_%s' % mobile)
+        # 2.如果已发送就提前响应,不执行后续代码
+        if flag:  # 如果if成立说明此手机号60秒内发过短信
+            return Response({'message': '频繁发送短信'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # 3.生成短信验证码
         sms_code = '%06d' % randint(0, 999999)
         logger.info(sms_code)
-        # 2.创建redis连接对象
-        redis_conn = get_redis_connection('verify_codes')
-        # 3.把验证码存储到redis中
+
+        # 4.把验证码存储到redis中
         # redis_conn.setex(key, 过期时间, value)
         redis_conn.setex('sms_%s' % mobile, 300, sms_code)
-        # 4.利用容联云通讯发短信
+        # 4.1 存储此手机号已发送短信标记
+        redis_conn.setex('send_flag_%s' % mobile, 60, 1)
+
+        # 5.利用容联云通讯发短信
         CCP().send_template_sms(mobile, [sms_code, 5], 1)
-        # 5.响应
+        # 6.响应
         return Response({'message': 'ok'})
