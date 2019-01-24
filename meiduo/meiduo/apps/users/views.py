b8b2923b19d13a5142700f33e6192052b54a6584
chao <chao@itcast.cn>
2019-01-24 09:29:55 +0800

    用户中心展示个人信息实现

--- meiduo/meiduo/apps/users/views.py
+++ meiduo/meiduo/apps/users/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserDetailSerializer
 from .models import User
 
 
-
 # Create your views here.
+# GET   /user/
+class UserDetailView(APIView):
+    """提供用户个人信息接口"""
+    permission_classes = [IsAuthenticated]  # 指定权限,必须是通过认证的用户才能访问此接口(就是当前本网站的登录用户)
+
+    def get(self, request):
+        user = request.user  # 获取本次请求的用户对象
+        serializer = UserDetailSerializer(user)
+        return Response(serializer.data)
+
 
 # POST /users/
 class UserView(CreateAPIView):
@@ -46,6 +56,3 @@ class MobileCountView(APIView):
             'mobile': mobile
         }
         return Response(data)
-
-
-
