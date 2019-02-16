b33a7e46205b7a4b9c2c1decf5bd7f82e5a2706f
chao <chao@itcast.cn>
2019-02-16 11:28:05 +0800

    支付成功后修改订单状态接口准备

--- meiduo/meiduo/apps/payment/urls.py
+++ meiduo/meiduo/apps/payment/urls.py
@@ -1,1 +1,1 @@
-from django.conf.urls import urlfrom . import viewsurlpatterns = [    # 生成支付登录url    url(r'^orders/(?P<order_id>\d+)/payment/$', views.PaymentView.as_view()),]
+from django.conf.urls import urlfrom . import viewsurlpatterns = [    # 生成支付登录url    url(r'^orders/(?P<order_id>\d+)/payment/$', views.PaymentView.as_view()),    # 支付成功后面修改订单    url(r'^payment/status/$', views.PaymentStatusView.as_view()),]
