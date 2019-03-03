cb9f44c18a6febd821cfaefdd52d47fe012549f4
catplane <nihaoyawoshixixida@gmail.com>
2019-03-03 20:22:35 +0800

    guguji6

--- meiduo/meiduo/settings/dev.py
+++ meiduo/meiduo/settings/dev.py
@@ -353,7 +353,7 @@ HAYSTACK_CONNECTIONS = {
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 # 配置读写分离
-DATABASE_ROUTERS = ['meiduo_mall.utils.db_router.MasterSlaveDBRouter']
+DATABASE_ROUTERS = ['meiduo.utils.db_router.MasterSlaveDBRouter']
 
 
 STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'front_end_pc/static')
